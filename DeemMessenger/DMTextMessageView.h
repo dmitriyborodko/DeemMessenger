f2677ea2cdcd2e8e6d4f2c426dd4960fe8143572
ALS_Deem <dmitriyborodko@gmail.com>
2017-07-06 12:58:38 +0700

    init commit

---
+++ DeemMessenger/DMTextMessageView.h
@@ -0,0 +1,13 @@
+//
+//  DMTextMessageView.h
+//  DeemMessenger
+//
+//  Created by ALS_Deem on 06/07/2017.
+//  Copyright © 2017 wrk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMTextMessageView : UIView
+
+@end
---
+++ DeemMessenger/DMTextMessageView.m
@@ -0,0 +1,21 @@
+//
+//  DMTextMessageView.m
+//  DeemMessenger
+//
+//  Created by ALS_Deem on 06/07/2017.
+//  Copyright © 2017 wrk. All rights reserved.
+//
+
+#import "DMTextMessageView.h"
+
+@implementation DMTextMessageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
